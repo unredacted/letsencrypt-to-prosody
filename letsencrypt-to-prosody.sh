e5cf27a852cd02a3ceedf104035aa4821ea243c6
Lunar <lunar@cypherpunk.is>
2018-03-17 14:27:46 -0500

    Update letsencrypt-to-prosody.sh

--- letsencrypt-to-prosody.sh
+++ letsencrypt-to-prosody.sh
@@ -7,10 +7,14 @@ LE_DIR="/etc/letsencrypt/live/"
 CERTS="/etc/prosody/certs/"
 PROSODY="/etc/prosody/"
 
-   cp -rfL "${LE_DIR}${DIR1}/" "${CERTS}"
-   cp -rfL "${LE_DIR}${DIR2}/" "${CERTS}"
-   cp -rfL "${LE_DIR}${DIR3}/" "${CERTS}"
+cp -rfL "${LE_DIR}${DIR1}/" "${CERTS}"
+cp -rfL "${LE_DIR}${DIR2}/" "${CERTS}"
+cp -rfL "${LE_DIR}${DIR3}/" "${CERTS}"
    
-   chown -R prosody:prosody "${PROSODY}"
-   chmod -R 700 "${CERTS}"
-   prosodyctl reload
+chown -R prosody:prosody "${PROSODY}"
+chmod -R 700 "${CERTS}"
+prosodyctl reload
+
+# Make sure mod_tls and mod_http get reloaded
+{ echo "module:reload('tls')"; sleep 1; } | telnet localhost 5582
+{ echo "module:reload('http')"; sleep 1; } | telnet localhost 5582
